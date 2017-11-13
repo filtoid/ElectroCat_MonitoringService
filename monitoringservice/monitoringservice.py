5c606531180581ed76a03fb8c5905f984db8120e
Phil Jeffes <philjeffes@hotmail.com>
2017-11-13 00:38:46 +0000

    Added in the actual reading upload

--- monitoringservice/monitoringservice.py
+++ monitoringservice/monitoringservice.py
@@ -1,6 +1,11 @@
+import httplib2, urllib
+from datetime import datetime
+import json 
+
 class MonitoringService(object):
     key = None
     type_ = "generic"
+    http = None
 
     TEMPERATURE = "temperature"
     GENERIC = "generic"
@@ -11,7 +16,14 @@ class MonitoringService(object):
 
     def __init__(self, key=None, type_="generic"):
         self.key = key
+
+        if type_ in self.COLLECTION:
+            print("Unknown type {}".format(type_))
+            self.type_ = self.GENERIC
+            return
+
         self.type_ = type_
+        self.http = httplib2.Http()
 
     def set_key(self, key):
         self.key = key
@@ -24,7 +36,20 @@ class MonitoringService(object):
 
         self.type = type_
 
-    def  upload_reading():
-        # TODO: Call the actual API
-    
-        pass
+    def  upload_reading(self, reading):
+        if reading is None or reading == "":
+            print("Reading cannot be blank")
+            return
+
+        if self.key is None or self.key == "":
+            print("Key is not set")
+            return
+
+        headers={'X-Api-Key': self.key,'Content-type': 'application/json'}
+        time_out = '{0:%Y-%m-%d %H:%M:%S}'.format(datetime.now())
+        reading={'reading': reading,'time':str(time_out)}
+        body={'reading':reading}
+        self.http.request('https://electrocatstudios.com/api/reading', 
+                       method="POST", 
+                       headers=headers,
+                       body=json.dumps(body) )[1]
